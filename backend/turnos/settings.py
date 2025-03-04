c166fd2bc52424d1be2ca537cc7a2ced97fbcbd9
maximo-cobeaga <maximocobeaga@gmail.com>
2025-03-03 23:31:52 -0300

    Envio de mail de activacion de cuenta al registrarse Funcional + Variables de entorno

--- backend/turnos/settings.py
+++ backend/turnos/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+from dotenv import  load_dotenv
+
+load_dotenv()
 
 from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT, STATIC_URL, EMAIL_BACKEND
 
@@ -24,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u$s!=2ph+y=xrvqyvwnr4-2&9(u0i&9ut=-(t#jrml!$e5#u2@'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False')== 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -188,8 +191,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'reservapp.dev@gmail.com'
-# EMAIL_HOST_PASSWORD = 'inhfjrdfljdryvpr'
-# EMAIL_PORT = 587
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
